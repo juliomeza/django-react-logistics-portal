4935b064b47f00f693e3499e4e19daa90835b43d
Julio Meza <juliomeza@gmail.com>
2025-03-13 11:37:36 -0400

    ProtectedRoute migrado a ts

--- client/src/features/layout/ProtectedRoute.tsx
+++ client/src/features/layout/ProtectedRoute.tsx
@@ -1,10 +1,14 @@
+import React, { ReactNode, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useContext } from 'react';
 import AuthContext from '../auth/AuthContext';
 import MainLayout from './MainLayout';
 
-const ProtectedRoute = ({ children }) => {
-  const { user, loading } = useContext(AuthContext);
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { user, loading } = useContext(AuthContext) as any;
 
   if (loading) return <div>Loading...</div>;
   if (!user) return <Navigate to="/login" />;
