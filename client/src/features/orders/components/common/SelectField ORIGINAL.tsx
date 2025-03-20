e2596a4ffe9b0097c964a8a50e6778e34a55e2a9
Julio Meza <juliomeza@gmail.com>
2025-03-20 09:46:57 -0400

    Common SelectField mejorado

--- client/src/features/orders/components/common/SelectField ORIGINAL.tsx
+++ client/src/features/orders/components/common/SelectField ORIGINAL.tsx
@@ -2,23 +2,21 @@ import React from 'react';
 import { FormControl, InputLabel, Select, MenuItem, FormHelperText, FormControlProps } from '@mui/material';
 import { SelectChangeEvent } from '@mui/material/Select';
 
-// Definimos un tipo genérico T para las opciones
-interface SelectFieldProps<T> extends Omit<FormControlProps, 'onChange'> {
+interface SelectFieldProps extends Omit<FormControlProps, 'onChange'> {
   label: string;
   name: string;
-  value: string | number | undefined;
-  onChange: (event: SelectChangeEvent<string | number>, child: React.ReactNode) => void;
-  options: T[];
+  value: any;
+  onChange: (event: SelectChangeEvent<any>, child: React.ReactNode) => void;
+  options: any[];
   required?: boolean;
-  getOptionLabel: (option: T) => string;
-  getOptionValue: (option: T) => string | number;
+  getOptionLabel: (option: any) => string;
+  getOptionValue: (option: any) => string | number;
   id?: string;
   error?: boolean;
   helperText?: string;
 }
 
-// Usamos un componente genérico para trabajar con cualquier tipo de opciones
-const SelectField = <T,>({
+const SelectField: React.FC<SelectFieldProps> = ({
   label,
   name,
   value,
@@ -31,7 +29,7 @@ const SelectField = <T,>({
   error = false,
   helperText = '',
   ...props
-}: SelectFieldProps<T>): React.ReactElement => {
+}) => {
   const labelId = `${id}-label`;
 
   return (
@@ -41,7 +39,7 @@ const SelectField = <T,>({
         labelId={labelId}
         id={id}
         name={name}
-        value={value ?? ''}
+        value={value}
         onChange={onChange}
         label={label}
         MenuProps={{
