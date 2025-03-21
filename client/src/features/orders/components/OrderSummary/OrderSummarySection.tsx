756373b55b10f12f087e59fa34b3a048e6e555f6
Julio Meza <juliomeza@gmail.com>
2025-03-21 08:59:25 -0400

    Component OrderSummarySection mejorado

--- client/src/features/orders/components/OrderSummary/OrderSummarySection.tsx
+++ client/src/features/orders/components/OrderSummary/OrderSummarySection.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, SxProps, Theme } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 
 interface OrderSummarySectionProps {
   title: string;
   children: React.ReactNode;
+  sx?: SxProps<Theme>;
 }
 
 const OrderSummarySection: React.FC<OrderSummarySectionProps> = ({
   title,
   children,
+  sx,
 }) => {
   return (
     <>
-      <Typography variant="subtitle1" sx={{ mb: 1, mt: 2 }}>
+      <Typography variant="subtitle1" sx={{ mb: 1, mt: 2, ...sx }}>
         {title}
       </Typography>
       <Grid container spacing={2}>
